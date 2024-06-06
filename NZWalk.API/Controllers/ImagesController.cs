8d8804f31c8c8b20bec2eb12e9901ad40c6ddd67
DQuan97 <ngduyquan97@gmail.com>
2024-06-06 23:17:52 +0700

    Update Image Upload (Controller, Domain, Dto, IRepository)

--- NZWalk.API/Controllers/ImagesController.cs
+++ NZWalk.API/Controllers/ImagesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalk.API.Models.Domain;
 using NZWalk.API.Models.DTO;
+using NZWalk.API.Repositories;
 
 namespace NZWalk.API.Controllers
 {
@@ -8,14 +10,33 @@ namespace NZWalk.API.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository) 
+        {
+            this.imageRepository = imageRepository;
+        }
         //POST: api/Images/upload
+        [HttpPost]
+        [Route("upload")]
         public async Task<IActionResult> Upload([FromForm] ImagesUploadRequestDto request) 
         {
             ValidateFileUpload(request);
 
             if (ModelState.IsValid)
             {
+                var imageDomainModel = new Image
+                {
+                    File = request.File,
+                    FileExtension = Path.GetExtension(request.File.FileName),
+                    FileDescription = request.FileDescription,
+                    FileName = request.FileName,
+                    FileSizeInBytes = request.File.Length
+                };
                 //Use Repository to upload imgage
+                await imageRepository.Upload(imageDomainModel);
+
+                return Ok(imageDomainModel);
             }
 
             return BadRequest(ModelState);
@@ -23,14 +44,14 @@ namespace NZWalk.API.Controllers
 
         private void ValidateFileUpload(ImagesUploadRequestDto request) 
         {
-            var allowedExtensions = new string[] { "jpg",".png"};
+            var allowedExtensions = new string[] { ".jpg",".png"};
 
-            if (allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            if (!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
             {
                 ModelState.AddModelError("file","Unsupported file extension");
             }
 
-            if (request.File.Length > 0)
+            if (request.File.Length > 10485760)
             {
                 ModelState.AddModelError("file", "File size more then 10mb , please upload a smaller size file ");
             }
---
+++ NZWalk.API/Repositories/LocalPathRepository.cs
@@ -0,0 +1,40 @@
+﻿using NZWalk.API.Data;
+using NZWalk.API.Models.Domain;
+
+namespace NZWalk.API.Repositories
+{
+    public class LocalPathRepository : IImageRepository
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly NZWalksDBContext nZWalksDBContext;
+
+        public LocalPathRepository(IWebHostEnvironment webHostEnvironment,
+            IHttpContextAccessor httpContextAccessor,
+            NZWalksDBContext nZWalksDBContext)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.httpContextAccessor = httpContextAccessor;
+            this.nZWalksDBContext = nZWalksDBContext;
+        }
+         
+
+        public async Task<Image> Upload(Image image)
+        {
+            var localPathImage = Path.Combine(webHostEnvironment.ContentRootPath,"Images",$"{image.FileName}{image.FileExtension}");
+
+            //Upload File to local Path
+            using var stream = new FileStream(localPathImage,FileMode.Create);
+            await image.File.CopyToAsync(stream);
+
+            // https://localhost1234/images/images.jpb
+            var urlFilePath = $"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host}{httpContextAccessor.HttpContext.Request.PathBase}/Images/{image.FileName}{image.FileExtension}";
+            image.FilePath = urlFilePath;   
+            
+            await nZWalksDBContext.Images.AddAsync(image);
+            await nZWalksDBContext.SaveChangesAsync();
+
+            return image;
+        }
+    }
+}
